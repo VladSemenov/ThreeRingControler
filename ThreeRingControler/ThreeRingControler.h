f1dec287ab427ad3fbc9241e38e3631ec37724a7
Vladislav Semenov <siamionau.ulad@gmail.com>
2017-12-21 11:08:08 +0300

    Initial Commit

---
+++ ThreeRingControler/ThreeRingControler.h
@@ -0,0 +1,19 @@
+//
+//  ThreeRingControler.h
+//  ThreeRingControler
+//
+//  Created by Vladislav Semenov on 21.12.2017.
+//  Copyright © 2017 Vladislav Semenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeRingControler.
+FOUNDATION_EXPORT double ThreeRingControlerVersionNumber;
+
+//! Project version string for ThreeRingControler.
+FOUNDATION_EXPORT const unsigned char ThreeRingControlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeRingControler/PublicHeader.h>
+
+
